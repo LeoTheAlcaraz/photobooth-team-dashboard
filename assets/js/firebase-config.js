6482fec36e1b0f62824cf8f7587b976e4f524edd
LeoTheAlcaraz <leodalcarazjr@iskolarngbayan.pup.edu.ph>
2026-05-27 23:11:13 +0800

    Add committed Firebase config for deployment

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase config for the deployed app.
+// This is public client config, not a secret.
+window.__FIREBASE_CONFIG = {
+  apiKey: "AIzaSyApP64RrOc4wsM2JlCCyBnbmISjwLr2evs",
+  authDomain: "photobooth-io-team-workspace.firebaseapp.com",
+  projectId: "photobooth-io-team-workspace",
+  storageBucket: "photobooth-io-team-workspace.firebasestorage.app",
+  messagingSenderId: "364880045388",
+  appId: "1:364880045388:web:58b57601961f24d9025be9",
+  measurementId: "G-JLTJTCSLHT"
+};
